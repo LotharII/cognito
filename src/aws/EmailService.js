3e4312acf46d579a555395f6f61b6a177c26cddb
sunil biradar <sunilb0575@gmail.com>
2018-08-09 10:43:28 -0700

    New change to verification email with token (#9)

--- src/aws/EmailService.js
+++ src/aws/EmailService.js
@@ -13,7 +13,7 @@ exports.sendEmail = function(email, token, event, context) {
             Body: {
                 Html: {
                    //template or environment variable
-                  Data: `<h2>CWS-CARES Secure Account Verification</h2><h1>Verification Code</h1><div>Here is your Access Code:</div><h1>${token}</h1><div>This is a validation code, not a password.</div><div>Simply copy this code and paste it into the Account Verification input field.</div><br><br><div>Thanks for helping us verify your CWS-CARES account.</div><div>CWS-CARES</div>`
+                  Data: `<h2>CWS-CARES Secure Account Verification</h2><h1>Verification Code</h1><div>Here is your Verification Code:</div><h1>${token}</h1><div>This is a verification code, not a password.</div><div>Simply copy this code and paste it into the Account Verification input field.</div><br><br><div>Thanks for helping us verify your CWS-CARES account.</div><div>CWS-CARES</div>`
                 }
             },
             Subject: {
